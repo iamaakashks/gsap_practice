7a72cdb594adc3f3f59a6bc79bb5624fd1833873
IAmGit@06 <aakashkumarsumanaksgi7@gmail.com>
2024-08-11 07:34:55 +0530

    updated some images

--- gsapPractice/script.js
+++ gsapPractice/script.js
@@ -8,7 +8,14 @@ tl.from("h3", {
 
 tl.from("#main>h1", {
     x:-100,
-    duration:1,
+    duration:0.4,
     opacity:0,
-    stagger:0.3
+    stagger:0.2
+})
+tl.from(".img", {
+    x:100,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4,
+    rotate:45
 })
